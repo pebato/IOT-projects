fbee7983ba5bd08fe7a820d58c36d793298d0927
pebato <pedrodib2019@gmail.com>
2021-04-30 22:13:18 -0300

    commit #2, com algumas modificações

--- Jogo da Memoria/jogomemoria.ino
+++ Jogo da Memoria/jogomemoria.ino
@@ -9,150 +9,163 @@ Pedro Dib @2021
 inspirado em: http://jeknowledge.github.io/academy-articles/jogo-da-memoria
 */
 
-String recebido;
-String sequenciaNumerica = "";
-int led;
-long numeroGerado;
-char stateGame = 0;
-int sequencia;
-
+int ledPins[] = {7,8,9,10,11,12};     // Array contendo os pinos dos leds
+int numLeds = 6;                      // Quantos leds estão conectados, determina quantos leds são usados no jogo
+int gameState = 0;                    // Estado atual do jogo
+int lvl = 1;                          // Nível atual do jogo, sendo 0 - Início, 1 - Rodada e 2 - Game Over
 
 void setup() {
-  Serial.begin(9600);
-  for(int led = 8; led <= 10; led++){
-    pinMode(led, OUTPUT);
+  Serial.begin(9600);               // Inicia o serial
+  for(int i = 0; i < numLeds; i++){ // Para cada led, configura o pino para output
+    pinMode(ledPins[i], OUTPUT);
   } 
-  randomSeed(analogRead(0));
-
+  randomSeed(analogRead(0));        // Inicia o estado atual do gerador de números aleatórios
 }
+
 void loop() {
-  switch (stateGame) {
-    case 0:
-      Serial.println("INICIO");
-      Serial.println("Comecar? (s/n)");
-      leserial();
-      if (recebido.equalsIgnoreCase("s")){
-        stateGame = 1;
-        Serial.println("Jogo comecando...");
-        piscaled(1000,3);
+  switch (gameState) {    // Gerencia o estado atual do jogo
+    case 0: {
+      /*
+        Nesse estado, acontece os eventos:
+          - Mostra a mensagem de boas vindas no serial;
+          - Animação dos leds ciclo 5x;
+          - Pisca os leds 2x;
+          - Espera o input inicial do jogador atual;
+          - Muda o estado do jogo para 1 - Rodada.
+      */
+      Serial.println("Bem vindo ao Jogo da Memória!");
+      cicleLeds(5, 50);
+      blinkLeds(2);
+      Serial.println("Pressione qualquer tecla para começar!");
+      readSerial();
+      Serial.println("");
+      gameState = 1;
+      break;
+    }
+    case 1: {
+      /*
+        Nesse estado, acontece os eventos:
+          - Mostra o nível atual;
+          - Contagem antes de começar a rodada, mostrando sequencialmente as mensagems e piscando os leds;
+          - Gera a sequência dos leds e pisca os leds correspondentes;
+          - Depois de acabar de mostrar a sequência, espera a resposta do usuário;
+          - Caso a resposta seja igual á sequência gerada:
+            - Adiciona um ao nível atual;
+            - Mostra a mensagem que o jogador acertou;
+            - Animação dos leds ciclo 5x;
+            - Pisca os leds 2x.
+          - Caso a resposta não seja igual á sequência gerada:
+            - Mostra a mensagem que o jogador errou;
+            - Animação dos leds ciclo 5x, rápido;
+            - Pisca os leds 2x;
+            - Muda o estado do jogo para 2 - Game Over.
+      */
+      Serial.print("Nível ");
+      Serial.println(lvl);
+      Serial.println("Memorize...");
+      blinkLeds(2);
+      delay(500);
+      Serial.println("O...");
+      blinkLeds(2);
+      delay(500);
+      Serial.println("Padrão!");
+      Serial.println("");
+      String sequence = generateSequence(lvl * 2 + 1);
+      String aswner = readSerial();
+      //Serial.println(String("Desired: ") + sequence + String(" Received: ") + aswner);
+      Serial.println("");
+      if (aswner == sequence) {
+        lvl++;
+        Serial.println("Correto!");
+        Serial.println("");
+        cicleLeds(5, 50);
+        blinkLeds(2);
+        delay(500);
       } else {
-        stateGame = 3;
-        Serial.println("Jogo nao iniciado"); 
-        piscaled(300,5);     
+        Serial.println("Erroooou :(");
+        Serial.println("");
+        cicleLeds(5, 25);
+        blinkLeds(2);
+        delay(500);
+        gameState = 2;
       }
       break;
-    case 1:
-      Serial.println("Nivel 1");
-      geraSequencia(1000, 3); 
-      leserial();
-      if (recebido == sequenciaNumerica){
-        stateGame = 2;
-        Serial.println("Parabens! proximo nivel.");
-        piscaled(1000,3);
-      } else {
-        stateGame = 3;
-        Serial.println("Errou!"); 
-        piscaled(300,5);     
-      }
-      break;
-    case 2:
-      Serial.println("* Nivel 2 *");
-      geraSequencia(1000, 5); 
-      leserial();
-      if (recebido == sequenciaNumerica){
-        stateGame = 2;
-        Serial.println("Parabens! proximo nivel.");
-        piscaled(1000,3);
-      } else {
-        stateGame = 3;
-        Serial.println("Errou!"); 
-        piscaled(300,5);     
-      }
-      break;
-    case 3:
-      Serial.println("Game Over"); 
-      piscaled(100,5);
-      stateGame = 4;
-      break;
-    case 4:
-      Serial.println("jogar novamente? (s/n)"); 
-      leserial();
-      if (recebido.equalsIgnoreCase("s")){
-        stateGame = 1;
-        Serial.println("Jogo comecando...");
-        piscaled(1000,3);
-      } else {
-        stateGame = 3;
-        Serial.println("Jogo nao iniciado"); 
-        piscaled(300,5);     
-      }
-      break;
+    }
+    case 2: {
+      /*
+        Nesse estado, acontece os eventos:
+          - Mostra a mensagem de Game Over;
+          - Mostra quantas vezes o jogador acertou;
+          - Animação dos leds ciclo 10x, rápido;
+          - Muda o estado do jogo para 0 - Início
+      */
+      Serial.println("Game over");
+      Serial.print("Você acertou ");
+      Serial.print(lvl - 1);
+      Serial.println(" Vezes!");
+      Serial.println("");
+      cicleLeds(10, 25);
+      gameState = 0;
+    }
   }
 }
 
-void leserial(){
+// Ciclo entre os leds, com delay entre cada digitalWrite
+void cicleLeds(int times, int perLedDelay) {
+  for (int j = 0; j < times; j++) {     // Repete n vezes
+    for (int i = 0; i < numLeds; i++) { // Para cada led
+      digitalWrite(ledPins[i], HIGH);   // Acende o led
+      delay(perLedDelay);               // Espera o delay específicado
+    }
+    for (int i = 0; i < numLeds; i++) { // Para cada led
+      digitalWrite(ledPins[i], LOW);    // Apaga o led
+      delay(perLedDelay);               // Espera o delay específicado
+    }
+  }
+}
 
-  Serial.println("Insira sua resposta");
+// Pisca todos os leds de uma vez, sem delay entre cada digitalWrite
+void blinkLeds(int times){
+  for (int j = 0; j < times; j++) {     // Repete n vezes
+    for (int i = 0; i < numLeds; i++) { // Para cada led
+      digitalWrite(ledPins[i], HIGH);   // Acende o led
+    }
+    delay(100);                         // Espera 0,1 segundos
+    for (int i = 0; i < numLeds; i++) { // Para cada led
+      digitalWrite(ledPins[i], LOW);    // Apaga o led
+    }
+    delay(100);                         // Espera 0,1 segundos
+  }
+}
 
+// Gera uma sequência de determinado tamanho
+String generateSequence(int size) {
+  String sequenceStr = "";                  // Sequência gerada
+  
+  for (int i = 0; i < size; i++) {          // Para o tamanho específicado
+    int number = random(numLeds);           // Gera um número entre 0 - (Número de leds - 1) (o número máximo é descartado).
+    sequenceStr += (number + 1);            // Adiciona o número para a sequência
+
+    digitalWrite(ledPins[number], HIGH);    // Acende o respectivo led
+    delay(500);                             // Espera meio segundo
+    digitalWrite(ledPins[number], LOW);     // Apaga o respectivo led
+    delay(500);                             // Espera meio segundo
+  }
+  
+  return sequenceStr;
+}
+
+// Lê a porta serial
+String readSerial(){
+  Serial.println("Insira sua resposta: ");
+
+  // Aguardar a resposta do usuario
   while (Serial.available() == 0)
   {}
 
+  // Retorna o valor introduzido pelo utilizador no Serial Monitor
   if (Serial.available())
   {
-    recebido = Serial.readString();
+    return Serial.readString();
   }
 }
-
-void piscaled(int tempo, int vezes){
-  for(int i = 0;i < vezes; i++){
-    for(int led = 8; led <= 10; led++){
-      digitalWrite(led, HIGH);
-    }
-    delay(tempo);
-    for(int led = 8; led <= 10; led++){
-      digitalWrite(led, LOW);
-    }
-    delay(tempo);
-  }
-}
-
-void geraSequencia (int tempo, int sequencia){
-  int ordemLeds[sequencia];
-
-  // Gerar sequencia aleatoria
-  for (int i = 0; i < sequencia; i++){
-    numeroGerado = random(1, 4);
-    ordemLeds[i] = numeroGerado;
-  }
-
-  // Inicialmente, a String sequenciaNumerica é uma String vazia
-  sequenciaNumerica = "";
-
-  // Pisca os LEDs na sequencia gerada
-  for (int j = 0; j < sequencia; j++){
-    led = ordemLeds[j];
-    if (led == 1){
-      digitalWrite(8, HIGH);
-      delay(tempo);
-      digitalWrite(8, LOW);
-      delay(tempo);
-    }else if (led == 2){
-      digitalWrite(9, HIGH);
-      delay(tempo);
-      digitalWrite(9, LOW);
-      delay(tempo);
-    }else if (led == 3){
-      digitalWrite(10, HIGH);
-      delay(tempo);
-      digitalWrite(10, LOW);
-      delay(tempo);
-    }
-    // Converte a lista numa String   
-    sequenciaNumerica = sequenciaNumerica + led;
-  }
-}
-
-
-
-
-
